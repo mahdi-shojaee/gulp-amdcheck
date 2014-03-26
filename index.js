56f87975f42876fa060ec57d939cfd526a2ced40
Mehdi Shojaei <msh6180339@gmail.com>
2014-03-27 01:34:44 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+var through = require('through2');
+var gutil = require('gulp-util');
+var es = require('event-stream');
+var amdextract = require('amdextract');
+var PluginError = gutil.PluginError;
+
+const PLUGIN_NAME = 'gulp-amdcheck';
+
+function gulpAmdCheck(options) {
+  options = options || {};
+
+  options.logFilePath = options.logFilePath || true;
+  options.logModuleId = options.logModuleId || false;
+  options.logDependencyPaths = options.logDependencyPaths || false;
+  options.logDependencyNames = options.logDependencyNames || false;
+  options.logUnusedDependencyPaths = options.logUnusedDependencyPaths || true;
+  options.logUnusedDependencyNames = options.logUnusedDependencyNames || false;
+  options.removeUnusedDependencies = options.removeUnusedDependencies || true;
+
+  options.logFilePath = options.logFilePath || options.logDependencyPaths || options.logDependencyNames || options.logUnusedDependencyPaths || options.logUnusedDependencyNames;
+
+  var stream = through.obj(function(file, enc, callback) {
+    if (file.isNull()) {
+      this.push(file);
+      return callback();
+    }
+
+    if (file.isBuffer()) {
+      var output = amdextract.parse(file.contents.toString(), options);
+
+      if (options.removeUnusedDependencies) {
+        file.contents = new Buffer(output.optimizedContent);
+      }
+
+      this.push(file);
+      return callback();
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+      return callback();
+    }
+  });
+
+  return stream;
+};
+
+module.exports = gulpAmdCheck;
---
+++ test/test.js
@@ -0,0 +1,22 @@
+var should = require('should');
+var File = require('vinyl');
+var amdcheck = require('..');
+
+describe('gulp-amdcheck', function() {
+  describe('in buffer mode', function() {
+    it('should optimize module', function(done) {
+      var bufferFile = new File({
+        contents: new Buffer('define(["p1"], function(a) {})')
+      });
+
+      var stream = amdcheck();
+      stream.write(bufferFile);
+
+      stream.once('data', function(file) {
+        file.isBuffer().should.be.equal(true);
+        file.contents.toString().should.be.equal('define([], function() {})');
+        done();
+      });
+    });
+  });
+});
